7cb6a7fe8a8d4e8300cb8b34609d3fd44d05e441
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2024-03-18 10:47:19 -0700

    possible adress bar fix

--- index.js
+++ index.js
@@ -7,10 +7,3 @@ var svg = d3.select("svg")
   function selectOption(option) {
     document.querySelector('.dropbtn').innerText = option;
   }
-
-  function navigateTo(page) {
-    // Handle navigation without reloading the page
-    // For example, you can show/hide content based on the selected page
-    // Here, we're just logging the page name
-    console.log('Navigating to:', page);
-}
