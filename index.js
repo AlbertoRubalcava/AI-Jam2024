ac2739fe7992aeaef3fe552a0585a2882cddee15
AlbertoSandovalCSUN <sandovalalbert2004@gmail.com>
2024-03-18 16:24:03 -0700

    Moved div for unit boxes around and added a graph

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-var svgWidth = 200, svgHeight = 200;
-var svg = d3.select("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight)
-  .attr("class", "svg-container");
+var canvas = d3.select("#main").append("svg")
+  .attr("width", 500)
+  .attr("height", 500)
+  .append("g")
+    .attr("transform","translate(50,50)");
 
+<<<<<<< Updated upstream
   function selectOption(option) {
     document.querySelector('.dropbtn').innerText = option;
   }
+=======
+var tree = d3.layout.tree()
+  .size([400,400]);
+
+d3.json("mydata.json", function (data) {
+  var nodes = tree.nodes(data);
+  var links = tree.links(nodes);
+  
+  var node = canvas.selectAll(".node")
+    .data(nodes)
+    .enter()
+    .append("g")
+      .attr("class", "node")
+      .attr("transform", function (d) {return "translate(" + d.y + "," +d.x+")";})
+    
+  node.append("circle")
+    .attr("r",5)
+    .attr("fill", "steelblue");
+
+  node.append("text")
+    .text(function (d) {return d.name});
+
+  var diagonal = d3.svg.diagonal()
+    .projection(function (d) {return [d.y,d.x];})
+
+  canvas.selectAll(".link")
+    .data(links)
+    .enter()
+    .append("path")
+    .attr("class", "link")
+    .attr("fill","none")
+    .attr("stroke", "black")
+    .attr("d", diagonal);
+})
+>>>>>>> Stashed changes
